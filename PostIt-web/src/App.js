c63856c3c01aa41716a0b9c03fdb6d51b0fb0090
Jheral-AB <109878408+Jheral-AB@users.noreply.github.com>
2022-07-23 18:52:39 -0400

    Aggregation of project "Post It"

---
+++ PostIt-web/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Lista from './components/Lista';
+
+
+function App() {
+  return (
+    <div className='container mt-4'>
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
